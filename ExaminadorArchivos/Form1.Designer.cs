8c356478b66d95f02ba1f6a6b195448d510ae857
La lechuza <aldair.rc70@hotmail.com>
2021-09-10 01:24:41 -0500

    Primer commit

---
+++ ExaminadorArchivos/Form1.Designer.cs
@@ -0,0 +1,220 @@
+﻿
+namespace ExaminadorArchivos
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.lstArchivosA = new System.Windows.Forms.ListBox();
+            this.btnBuscarA = new System.Windows.Forms.Button();
+            this.txtRutaA = new System.Windows.Forms.TextBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.lstArchivosB = new System.Windows.Forms.ListBox();
+            this.btnBuscarB = new System.Windows.Forms.Button();
+            this.txtRutaB = new System.Windows.Forms.TextBox();
+            this.btnComparar = new System.Windows.Forms.Button();
+            this.lstResultado = new System.Windows.Forms.ListBox();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.btnDescargar = new System.Windows.Forms.Button();
+            this.fbdEstablecerRuta = new System.Windows.Forms.FolderBrowserDialog();
+            this.lblArchivosEncontradosA = new System.Windows.Forms.Label();
+            this.lblArchivosEncontradosB = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.lblArchivosEncontradosA);
+            this.groupBox1.Controls.Add(this.lstArchivosA);
+            this.groupBox1.Controls.Add(this.btnBuscarA);
+            this.groupBox1.Controls.Add(this.txtRutaA);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(279, 373);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "¿Qué libros de aquí....?";
+            // 
+            // lstArchivosA
+            // 
+            this.lstArchivosA.FormattingEnabled = true;
+            this.lstArchivosA.Location = new System.Drawing.Point(20, 55);
+            this.lstArchivosA.Name = "lstArchivosA";
+            this.lstArchivosA.Size = new System.Drawing.Size(244, 290);
+            this.lstArchivosA.TabIndex = 2;
+            // 
+            // btnBuscarA
+            // 
+            this.btnBuscarA.Location = new System.Drawing.Point(212, 27);
+            this.btnBuscarA.Name = "btnBuscarA";
+            this.btnBuscarA.Size = new System.Drawing.Size(52, 23);
+            this.btnBuscarA.TabIndex = 1;
+            this.btnBuscarA.Text = "...";
+            this.btnBuscarA.UseVisualStyleBackColor = true;
+            this.btnBuscarA.Click += new System.EventHandler(this.btnBuscarA_Click);
+            // 
+            // txtRutaA
+            // 
+            this.txtRutaA.Location = new System.Drawing.Point(20, 29);
+            this.txtRutaA.Name = "txtRutaA";
+            this.txtRutaA.Size = new System.Drawing.Size(186, 20);
+            this.txtRutaA.TabIndex = 0;
+            this.txtRutaA.KeyDown += new System.Windows.Forms.KeyEventHandler(this.txtRutaA_KeyDown);
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.lblArchivosEncontradosB);
+            this.groupBox2.Controls.Add(this.lstArchivosB);
+            this.groupBox2.Controls.Add(this.btnBuscarB);
+            this.groupBox2.Controls.Add(this.txtRutaB);
+            this.groupBox2.Location = new System.Drawing.Point(299, 12);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(279, 373);
+            this.groupBox2.TabIndex = 3;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "¿...no tengo aquí?";
+            // 
+            // lstArchivosB
+            // 
+            this.lstArchivosB.FormattingEnabled = true;
+            this.lstArchivosB.Location = new System.Drawing.Point(20, 55);
+            this.lstArchivosB.Name = "lstArchivosB";
+            this.lstArchivosB.Size = new System.Drawing.Size(244, 290);
+            this.lstArchivosB.TabIndex = 2;
+            // 
+            // btnBuscarB
+            // 
+            this.btnBuscarB.Location = new System.Drawing.Point(212, 27);
+            this.btnBuscarB.Name = "btnBuscarB";
+            this.btnBuscarB.Size = new System.Drawing.Size(52, 23);
+            this.btnBuscarB.TabIndex = 1;
+            this.btnBuscarB.Text = "...";
+            this.btnBuscarB.UseVisualStyleBackColor = true;
+            this.btnBuscarB.Click += new System.EventHandler(this.btnBuscarB_Click);
+            // 
+            // txtRutaB
+            // 
+            this.txtRutaB.Location = new System.Drawing.Point(20, 29);
+            this.txtRutaB.Name = "txtRutaB";
+            this.txtRutaB.Size = new System.Drawing.Size(186, 20);
+            this.txtRutaB.TabIndex = 0;
+            this.txtRutaB.KeyDown += new System.Windows.Forms.KeyEventHandler(this.txtRutaB_KeyDown);
+            // 
+            // btnComparar
+            // 
+            this.btnComparar.Location = new System.Drawing.Point(12, 391);
+            this.btnComparar.Name = "btnComparar";
+            this.btnComparar.Size = new System.Drawing.Size(566, 23);
+            this.btnComparar.TabIndex = 4;
+            this.btnComparar.Text = "Comparar";
+            this.btnComparar.UseVisualStyleBackColor = true;
+            this.btnComparar.Click += new System.EventHandler(this.btnComparar_Click);
+            // 
+            // lstResultado
+            // 
+            this.lstResultado.FormattingEnabled = true;
+            this.lstResultado.Location = new System.Drawing.Point(12, 420);
+            this.lstResultado.Name = "lstResultado";
+            this.lstResultado.Size = new System.Drawing.Size(566, 147);
+            this.lstResultado.TabIndex = 5;
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(12, 573);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(566, 23);
+            this.progressBar1.TabIndex = 6;
+            // 
+            // btnDescargar
+            // 
+            this.btnDescargar.Location = new System.Drawing.Point(12, 573);
+            this.btnDescargar.Name = "btnDescargar";
+            this.btnDescargar.Size = new System.Drawing.Size(566, 23);
+            this.btnDescargar.TabIndex = 7;
+            this.btnDescargar.Text = "Descargar Rutas";
+            this.btnDescargar.UseVisualStyleBackColor = true;
+            // 
+            // lblArchivosEncontradosA
+            // 
+            this.lblArchivosEncontradosA.AutoSize = true;
+            this.lblArchivosEncontradosA.Location = new System.Drawing.Point(20, 352);
+            this.lblArchivosEncontradosA.Name = "lblArchivosEncontradosA";
+            this.lblArchivosEncontradosA.Size = new System.Drawing.Size(0, 13);
+            this.lblArchivosEncontradosA.TabIndex = 3;
+            // 
+            // lblArchivosEncontradosB
+            // 
+            this.lblArchivosEncontradosB.AutoSize = true;
+            this.lblArchivosEncontradosB.Location = new System.Drawing.Point(17, 352);
+            this.lblArchivosEncontradosB.Name = "lblArchivosEncontradosB";
+            this.lblArchivosEncontradosB.Size = new System.Drawing.Size(0, 13);
+            this.lblArchivosEncontradosB.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(590, 606);
+            this.Controls.Add(this.btnDescargar);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.lstResultado);
+            this.Controls.Add(this.btnComparar);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.ListBox lstArchivosA;
+        private System.Windows.Forms.Button btnBuscarA;
+        private System.Windows.Forms.TextBox txtRutaA;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.ListBox lstArchivosB;
+        private System.Windows.Forms.Button btnBuscarB;
+        private System.Windows.Forms.TextBox txtRutaB;
+        private System.Windows.Forms.Button btnComparar;
+        private System.Windows.Forms.ListBox lstResultado;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Button btnDescargar;
+        private System.Windows.Forms.FolderBrowserDialog fbdEstablecerRuta;
+        private System.Windows.Forms.Label lblArchivosEncontradosA;
+        private System.Windows.Forms.Label lblArchivosEncontradosB;
+    }
+}
+
---
+++ ExaminadorArchivos/Form1.cs
@@ -0,0 +1,267 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.IO;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace ExaminadorArchivos
+{
+    public partial class Form1 : Form
+    {
+        string[] rutasA, rutasB;
+        FileSystemWatcher watcherA = null;
+        FileSystemWatcher watcherB = null;
+
+        enum OpcionRuta
+        {
+            RutaA = 1,
+            RutaB = 2
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnBuscarA_Click(object sender, EventArgs e)
+        {
+            if (fbdEstablecerRuta.ShowDialog() == DialogResult.OK)
+            {
+                string ruta = fbdEstablecerRuta.SelectedPath;
+                if (ruta != "")
+                {
+                    try
+                    {
+                        rutasA = BuscarArchivos(ruta);
+                        ListarArchivosEncontrados(rutasA, lstArchivosA);
+                        GenerarEventosEscucha(OpcionRuta.RutaA, ruta);
+                        txtRutaA.Text = ruta;
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show(ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+                }
+            }
+        }
+
+        private void txtRutaA_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                string ruta = txtRutaA.Text;
+                if (ruta.Length > 0)
+                {
+                    try
+                    {
+                        rutasA = BuscarArchivos(ruta);
+                        ListarArchivosEncontrados(rutasA, lstArchivosA);
+                        GenerarEventosEscucha(OpcionRuta.RutaA, ruta);
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show(ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+                }
+            }
+        }
+
+        private void btnBuscarB_Click(object sender, EventArgs e)
+        {
+            if (fbdEstablecerRuta.ShowDialog() == DialogResult.OK)
+            {
+                string ruta = fbdEstablecerRuta.SelectedPath;
+                if (ruta != "")
+                {
+                    try
+                    {
+                        rutasB = BuscarArchivos(ruta);
+                        ListarArchivosEncontrados(rutasB, lstArchivosB);
+                        GenerarEventosEscucha(OpcionRuta.RutaB, ruta);
+                        txtRutaB.Text = ruta;
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show(ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+                }
+            }
+        }
+
+        private string[] BuscarArchivos(string ruta)
+        {
+            try
+            {
+                return Directory.GetFiles(ruta, "*.pdf", SearchOption.AllDirectories);
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        private void txtRutaB_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                string ruta = txtRutaB.Text;
+                if (ruta.Length > 0)
+                {
+                    try
+                    {
+                        rutasB = BuscarArchivos(ruta);
+                        ListarArchivosEncontrados(rutasB, lstArchivosB);
+                        GenerarEventosEscucha(OpcionRuta.RutaB, ruta);
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show(ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+                }
+            }
+        }
+
+        private void btnComparar_Click(object sender, EventArgs e)
+        {
+            lstResultado.Items.Clear();
+            string nombreArchivo = "";
+            foreach (string rutaA in rutasA)
+            {
+                nombreArchivo = Path.GetFileName(rutaA);
+                if (!EncontrarArchivoEnDestino(nombreArchivo))
+                {
+                    lstResultado.Items.Add(nombreArchivo);
+                }
+            }
+        }
+
+        private void ListarArchivosEncontrados(string[] rutas, ListBox listbox)
+        {
+            listbox.Items.Clear();
+            foreach (string ruta in rutas)
+            {
+                listbox.Items.Add(Path.GetFileName(ruta));
+            }
+            var controlParent = listbox.Parent;
+            Label control = controlParent.Controls.OfType<Label>().FirstOrDefault();
+            control.Text = "Documentos encontrados: " + rutas.Length;
+        }
+
+        private int RetornarIndiceArchivo(string ruta, string[] rutas)
+        {
+            int item = -1;
+            for (int i = 0; i < rutas.Length; i++)
+            {
+                if (ruta == rutas[i])
+                {
+                    item = i;
+                    break;
+                }
+            }
+            return item;
+        }
+
+        private bool EncontrarArchivoEnDestino(string nombreArchivo)
+        {
+            bool encontro = false;
+            string archivoB;
+            foreach (string rutaB in rutasB)
+            {
+                archivoB = Path.GetFileName(rutaB);
+                if (nombreArchivo == archivoB)
+                {
+                    encontro = true;
+                    break;
+                }
+            }
+            return encontro;
+        }
+
+        private void GenerarEventosEscucha(OpcionRuta opcionRuta, string ruta)
+        {
+            if (opcionRuta == OpcionRuta.RutaA)
+            {
+                watcherA = new FileSystemWatcher(ruta);
+                watcherA.IncludeSubdirectories = true;
+                watcherA.NotifyFilter = NotifyFilters.Attributes |
+                                       NotifyFilters.CreationTime |
+                                       NotifyFilters.DirectoryName |
+                                       NotifyFilters.FileName |
+                                       NotifyFilters.LastWrite |
+                                       NotifyFilters.LastAccess |
+                                       NotifyFilters.Security;
+                watcherA.Filter = "*.pdf";
+                //watcherA.Changed += new FileSystemEventHandler(listenerActualizarA);
+                watcherA.Created += new FileSystemEventHandler(listenerAdicionarA);
+                watcherA.Deleted += new FileSystemEventHandler(listenerBorrarA);
+                watcherA.Renamed += new RenamedEventHandler(listenerRenombrarA);
+                watcherA.EnableRaisingEvents = true;
+            }
+            else
+            {
+                watcherB = new FileSystemWatcher(ruta);
+                watcherB.IncludeSubdirectories = true;
+                watcherB.NotifyFilter = NotifyFilters.Attributes |
+                                       NotifyFilters.CreationTime |
+                                       NotifyFilters.DirectoryName |
+                                       NotifyFilters.FileName |
+                                       NotifyFilters.LastWrite |
+                                       NotifyFilters.LastAccess |
+                                       NotifyFilters.Security;
+                watcherB.Filter = "*.pdf";
+                watcherB.Changed += new FileSystemEventHandler(listenerB);
+                watcherB.Created += new FileSystemEventHandler(listenerB);
+                watcherB.Deleted += new FileSystemEventHandler(listenerB);
+                watcherB.Renamed += new RenamedEventHandler(renamedB);
+                watcherB.EnableRaisingEvents = true;
+            }
+        }
+
+        #region Eventos
+        public void listenerBorrarA(object source, FileSystemEventArgs e)
+        {
+            string rutaArchivo = e.FullPath;
+            int item = RetornarIndiceArchivo(rutaArchivo, rutasA);
+            rutasA = rutasA.Where(x => x != rutaArchivo).ToArray();
+            lstArchivosA.Invoke(new Action(() => lstArchivosA.Items.RemoveAt(item)));
+            lblArchivosEncontradosA.Invoke(new Action(() => lblArchivosEncontradosA.Text = "Documentos encontrados: " + rutasA.Length));
+        }
+
+        public void listenerAdicionarA(object source, FileSystemEventArgs e)
+        {
+            string rutaArchivo = e.FullPath;
+            List<string> tempList = rutasA.ToList();
+            tempList.Add(rutaArchivo);
+            rutasA = tempList.ToArray();
+            lstArchivosA.Invoke(new Action(() => lstArchivosA.Items.Add(Path.GetFileName(rutaArchivo))));
+            lblArchivosEncontradosA.Invoke(new Action(() => lblArchivosEncontradosA.Text = "Documentos encontrados: " + rutasA.Length));
+        }
+
+        public void listenerRenombrarA(object source, RenamedEventArgs e)
+        {
+            string rutaArchivoAnterior = e.OldFullPath;
+            string rutaArchivoActual = e.FullPath;
+            int item = RetornarIndiceArchivo(rutaArchivoAnterior, rutasA);
+            if (item >= 0)
+            {
+                rutasA[item] = rutaArchivoActual;
+                lstArchivosA.Invoke(new Action(() => lstArchivosA.Items[item] = Path.GetFileName(rutaArchivoActual)));
+            }
+        }
+
+        public void listenerB(object source, FileSystemEventArgs e)
+        {
+            string rutaArchivo = e.FullPath;
+            int item = RetornarIndiceArchivo(rutaArchivo, rutasB);
+            rutasB = rutasB.Where(x => x != rutaArchivo).ToArray();
+            lstArchivosB.Invoke(new Action(() => lstArchivosB.Items.RemoveAt(item)));
+        }
+
+        public void renamedB(object source, RenamedEventArgs e)
+        {
+            MessageBox.Show("Acción detectada");
+        }
+        #endregion
+    }
+}
